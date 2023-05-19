4ac9451cef15acc270d2e2173a202e621f02bacc
Manjot Virdi <virdi_manjot@hotmail.com>
2023-05-19 16:35:51 -0700

    add users seed data

---
+++ seed_data/users.ts
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        id: "174875ee-e638-4dea-9857-40f861c9774c",
+        username: "kevin_rodrigues",
+        password: "password1",
+        email: "1something@something.com",
+        profileImg: "kawe-rodrigues-Xnk_LLrltkc-unsplash.jpg",
+        name: "Kevin Rodrigues",
+        about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        setup: "Nikon D3500 DSLR w/ Nikon 50mm f/1.8G AF-S NIKKOR FX Lens",
+    },
+    {
+        id: "184875ee-e638-4dea-9857-40f861c9774c",
+        username: "sandra_g",
+        password: "password2",
+        email: "2something@something.com",
+        profileImg: "sandro-g-photography-QvgQizteOJE-unsplash.jpg",
+        name: "Sandra G",
+        about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        setup: "Nikon D3500 DSLR w/ Nikon 50mm f/1.8G AF-S NIKKOR FX Lens",
+    },
+    {
+        id: "194875ee-e638-4dea-9857-40f861c9774c",
+        username: "riz_ahmed",
+        password: "password3",
+        email: "3something@something.com",
+        profileImg: "sina-rezakhani--crLLu9epYs-unsplash.jpg",
+        name: "Riz Ahmed",
+        about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        setup: "Nikon D3500 DSLR w/ Nikon 50mm f/1.8G AF-S NIKKOR FX Lens",
+    },
+];

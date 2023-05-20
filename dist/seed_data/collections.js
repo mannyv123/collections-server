7e7584f41940b0c4ed4b2bdc691e645b55b83c31
Manjot Virdi <virdi_manjot@hotmail.com>
2023-05-20 11:32:05 -0700

    add seed data

---
+++ dist/seed_data/collections.js
@@ -0,0 +1,15 @@
+"use strict";
+module.exports = [
+    {
+        id: "134875ee-e638-4dea-9857-40f861c9774c",
+        title: "My time in Croatia!",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        user_id: "174875ee-e638-4dea-9857-40f861c9774c",
+    },
+    {
+        id: "144875ee-e638-4dea-9857-40f861c9774c",
+        title: "My time in Turkey!",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        user_id: "174875ee-e638-4dea-9857-40f861c9774c",
+    },
+];

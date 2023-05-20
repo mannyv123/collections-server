8f6c097b537c49ea0ca922c4c25ae729583bf7bc
Manjot Virdi <virdi_manjot@hotmail.com>
2023-05-20 13:54:25 -0700

    update users migration file

--- migrations/20230520202756_users.ts
+++ migrations/20230520202756_users.ts
@@ -1,10 +1,21 @@
 import { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("users", (table) => {
+        table.uuid("id").primary();
+        table.string("username").notNullable();
+        table.string("password").notNullable();
+        table.string("email").notNullable();
+        table.string("first_name").notNullable();
+        table.string("last_name").notNullable();
+        table.string("about");
+        table.string("setup");
+        table.string("profile_img").notNullable;
+        table.string("cover_img").notNullable;
+        table.timestamps(true, true);
+    });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable("users");
 }
-

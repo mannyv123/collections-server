6b639ec980d78d73f9e852480d50280e4daf9ccb
Manjot Virdi <virdi_manjot@hotmail.com>
2023-06-16 10:20:31 -0700

    update seed data

--- seed_data/collections.ts
+++ seed_data/collections.ts
@@ -1,14 +1,7 @@
 const collectionsData = [
     {
         id: "134875ee-e638-4dea-9857-40f861c9774c",
-        title: "My time in Croatia!",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        user_id: "174875ee-e638-4dea-9857-40f861c9774c",
-    },
-    {
-        id: "144875ee-e638-4dea-9857-40f861c9774c",
-        title: "My time in Turkey!",
+        title: "SF Trip",
         description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         user_id: "174875ee-e638-4dea-9857-40f861c9774c",

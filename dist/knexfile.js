5995e12513fb6e63ca287f42ad64a3dec01ccea9
Manjot Virdi <virdi_manjot@hotmail.com>
2023-06-17 11:23:43 -0700

    Update knexfile.ts to pull host from .env

--- dist/knexfile.js
+++ dist/knexfile.js
@@ -30,7 +30,7 @@ const config = {
     development: {
         client: "mysql2",
         connection: {
-            host: "127.0.0.1",
+            host: process.env.DB_LOCAL_HOST,
             database: process.env.DB_LOCAL_DBNAME,
             user: process.env.DB_LOCAL_USER,
             password: process.env.DB_LOCAL_PASSWORD,
